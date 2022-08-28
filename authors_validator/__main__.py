10aea0b2f06280f4e862bf7c187e23f26135ee8f
Felipe Flamarion <felipeflamari@gmail.com>
2022-08-28 20:43:48 -0300

    Refactor code

--- authors_validator/__main__.py
+++ authors_validator/__main__.py
@@ -1,35 +1,32 @@
 from typing import Dict, List
+from unittest import result
 
 import pandas
 from unidecode import unidecode
 
-RESULTS_FILE_NAME = "results.csv"
 
-
-def get_authors_names_list(dataframe: object, column_to_normalize: str) -> List[str]:
-    authors = dataframe[dataframe[column_to_normalize].notnull()][column_to_normalize]
+def get_names(df: object, column: str) -> List[str]:
+    authors = df[df[column].notnull()][column]
     return authors.values.tolist()
 
 
-def get_author_name_variations(author_name: str) -> List[str]:
-    variations = [author_name]
+def get_name_variations(name: str) -> List[str]:
+    variations = [name]
 
-    if not author_name.islower():
-        variations.append(author_name.lower())
+    if not name.islower():
+        variations.append(name.lower())
 
-    if not author_name.isupper():
-        variations.append(author_name.upper())
+    if not name.isupper():
+        variations.append(name.upper())
 
-    if not author_name.istitle():
-        variations.append(author_name.title())
-
-    # variations.append(f"{author_name.split()[0]} {author_name.split()[-1]}")
+    if not name.istitle():
+        variations.append(name.title())
 
     variations.extend(
         list(
             filter(
-                lambda author_name: author_name not in variations,
-                [unidecode(author_name) for author_name in variations],
+                lambda name: name not in variations,
+                [unidecode(name) for name in variations],
             )
         )
     )
@@ -37,51 +34,45 @@ def get_author_name_variations(author_name: str) -> List[str]:
     return variations
 
 
-def compare(author_name_variations: List[str], authors_names: List[str]) -> List[str]:
-    similars = []
+def analyze(name: str, variations: List[str], names: List[str]) -> Dict:
+    analysis_result = {"name": name, "similar_names": []}
 
-    for variation in author_name_variations:
-        for author_name in authors_names:
-            if variation == author_name:
-                similars.append(author_name)
+    for variation in variations:
+        for name in names:
+            if variation == name:
+                analysis_result["similar_names"].append(variation)
 
-    return similars
-
-
-def save_results(similarities: List[Dict], column_to_normalize: str):
-    data = []
-
-    for similarity in similarities:
-        data.append({column_to_normalize: similarity.get("author_name")})
-        for similar_name in similarity.get("similar_names"):
-            data.append({column_to_normalize: similar_name})
-
-    dataframe = pandas.DataFrame(data)
-    dataframe.to_csv(RESULTS_FILE_NAME, encoding="utf-8", index=False)
+    return analysis_result
 
 
-def run(file_path: str, column_to_normalize: str):
-    dataframe = pandas.read_csv(file_path)
-    authors_names = get_authors_names_list(dataframe, column_to_normalize)
+def prepare_results(results: List[Dict], column: str) -> List[Dict]:
+    prepared_results = []
 
-    similarities = []
-    for author_name in authors_names:
-        author_name_variations = get_author_name_variations(author_name)
-
-        similars = compare(author_name_variations, authors_names)
-        if len(similars) > 1:
-            similarities.append(
-                {
-                    "author_name": author_name,
-                    "similar_names": list(
-                        filter(lambda similar: similar != author_name, similars)
-                    ),
-                }
+    for result in results:
+        if len(result["similar_names"]) > 1:
+            prepared_results.extend(
+                [{column: similar_name} for similar_name in result["similar_names"]]
             )
 
-    save_results(similarities, column_to_normalize)
+    return prepared_results
+
+
+def save_results(results: List[Dict]):
+    dataframe = pandas.DataFrame(results)
+    dataframe.to_csv("results.csv", encoding="utf-8", index=False)
+
+
+def run(file_path: str, column: str):
+    df = pandas.read_csv(file_path)
+    all_names = get_names(df, column)
+
+    results = []
+    for name in all_names:
+        variations = get_name_variations(name)
+        results.append(analyze(name, variations, all_names))
+
+    save_results(prepare_results(results, column))
 
 
 if __name__ == "__main__":
-    # run(file_path="test-sample.csv", column_to_normalize="autores_unique")
-    run(file_path="sample.csv", column_to_normalize="autores_unique")
+    run(file_path="data.csv", column="autores_unique")
